c990de4faff56825e5c2be2713d7f8e5f4ef0c00
Kavimal Wijewardana <kavimal@thinkcube.com>
2013-06-16 08:34:09 +0530

    First commit. create the project with selected resolution sizes.

---
+++ gen/com/fkf/resturent/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.fkf.resturent;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
