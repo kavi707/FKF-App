173b06f40f6b2f764a687384dfaa0f01a03e2099
Kavimal Wijewardana <kavimal@thinkcube.com>
2013-10-14 23:40:14 +0530

    Add loading dialog to login part and add new images for application

--- src/com/fkf/resturent/templates/LoginActivity.java
+++ src/com/fkf/resturent/templates/LoginActivity.java
@@ -2,9 +2,10 @@ package com.fkf.resturent.templates;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,7 @@ import java.util.Map;
  * Created by kavi on 6/16/13.
  * Hold the login_layout view functionalities.
  * login_layout.xml Activity class
+ *
  * @author Kavimal Wijewardana <kavi707@gmail.com>
  */
 public class LoginActivity extends Activity {
@@ -30,6 +32,9 @@ public class LoginActivity extends Activity {
     private Button registerButton;
     private Button loginButton;
     private TextView browsRecipesTextView;
+
+    private Context context = this;
+    private Handler handler;
 
     private EditText usernameEditText;
     private EditText passwordEditText;
@@ -40,6 +45,10 @@ public class LoginActivity extends Activity {
     public static String LOGGED_USER;
     public static String LOGGED_USER_PASSWORD;
     public static int LOGGED_STATUS = 0;
+
+    private Map<String, String> loginResult = new HashMap<String, String>();
+    private boolean errorStatus = false;
+    private String errorMsg = "";
 
     private ApiConnector connector = new ApiConnector();
     private ActivityUserPermissionServices userPermissionServices = new ActivityUserPermissionServices();
@@ -72,38 +81,55 @@ public class LoginActivity extends Activity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String getUsername = usernameEditText.getText().toString();
-                String getPassword = passwordEditText.getText().toString();
 
                 if (userPermissionServices.isOnline(LoginActivity.this)) {
-                    Map<String, String> loginResult = connector.userLogin(getUsername, getPassword);
+                    progress = ProgressDialog.show(LoginActivity.this, "Login", "Check user and login to the systems. Please wait ...");
 
-                    if (loginResult.get("loginStatus").equals("1")) {
+                    handler = new Handler(context.getMainLooper());
 
-                        LOGGED_USER_ID = loginResult.get("userId");
-                        LOGGED_USER = loginResult.get("username");
-                        LOGGED_USER_PASSWORD = loginResult.get("password");
-                        LOGGED_STATUS = 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String getUsername = usernameEditText.getText().toString();
+                            String getPassword = passwordEditText.getText().toString();
+                            loginResult = connector.userLogin(getUsername, getPassword);
 
-                        /*progress = new ProgressDialog(LoginActivity.this);
-                        progress.setTitle("Loading");
-                        progress.setMessage("Wait while loading...");
-                        progress.show();*/
+                            if (loginResult.get("loginStatus").equals("1")) {
 
-                        localDatabaseSQLiteOpenHelper.insertLoginDetails(loginResult);
-                        //update the logged user's favorite recipes
-                        userPermissionServices.updateUserFavoriteRecipesFromServer(LoginActivity.this);
+                                LOGGED_USER_ID = loginResult.get("userId");
+                                LOGGED_USER = loginResult.get("username");
+                                LOGGED_USER_PASSWORD = loginResult.get("password");
+                                LOGGED_STATUS = 1;
 
-                        //progress.dismiss();
+                                localDatabaseSQLiteOpenHelper.insertLoginDetails(loginResult);
+                                //update the logged user's favorite recipes
+                                userPermissionServices.updateUserFavoriteRecipesFromServer(LoginActivity.this);
 
-                        Intent recipesIntent = new Intent(LoginActivity.this, RecipesActivity.class);
-                        startActivity(recipesIntent);
-                        finish();
-                    } else if (loginResult.get("loginStatus").equals("2")) {
-                        String errorMsg = loginResult.get("msg");
-                        Toast.makeText(getApplicationContext(),
-                                "Login failed. Because of " + errorMsg, Toast.LENGTH_LONG).show();
-                    }
+                                Intent recipesIntent = new Intent(LoginActivity.this, RecipesActivity.class);
+                                startActivity(recipesIntent);
+                                finish();
+
+                                errorStatus = false;
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        progress.dismiss();
+                                    }
+                                });
+
+                            } else if (loginResult.get("loginStatus").equals("2")) {
+                                progress.dismiss();
+                                errorStatus = true;
+                                errorMsg = loginResult.get("msg");
+                                Toast.makeText(getApplicationContext(),
+                                        "Login failed. Because of " + errorMsg, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Application is on offline mode. If need to login, please put your device to online", Toast.LENGTH_LONG).show();
                 }
             }
         });
