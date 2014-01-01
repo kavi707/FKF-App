005523ccda84fc81d395df0fe2edc746857ca156
Kavimal Wijewardana <kavimal@thinkcube.com>
2014-01-01 13:14:12 +0530

    Update the toast message when user click add favorite button in free-version

--- src/com/fkf/free/templates/SingleRecipeActivity.java
+++ src/com/fkf/free/templates/SingleRecipeActivity.java
@@ -405,7 +405,7 @@ public class SingleRecipeActivity extends Activity {
                         }
                     } else {
                         Toast.makeText(getApplicationContext(),
-                                "First you have to login to application. Then you can add favorite for your account.", Toast.LENGTH_LONG).show();
+                                "Buy commercial version to create account and add your own favorite recipes.", Toast.LENGTH_LONG).show();
                     }
                 } else {
                     Toast.makeText(getApplicationContext(),
