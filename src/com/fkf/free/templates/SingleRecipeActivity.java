b400f02690faa4fb5aa2eeff5f906ae9eab7d3b8
Kavimal Wijewardana <kavimal@thinkcube.com>
2014-01-02 22:18:35 +0530

    Changed the order of the ingredient placement

--- src/com/fkf/free/templates/SingleRecipeActivity.java
+++ src/com/fkf/free/templates/SingleRecipeActivity.java
@@ -315,9 +315,9 @@ public class SingleRecipeActivity extends Activity {
                             for (int k = 0; k < itemsJsonArray.length(); k++) {
                                 itemJsonObj = itemsJsonArray.getJSONObject(k);
 
-                                ingredientString = ingredientString + itemJsonObj.getString("name");
+                                ingredientString = ingredientString + itemJsonObj.getString("unit");
                                 if (!itemJsonObj.getString("unit").equals("null")) {
-                                    ingredientString = ingredientString + " " + itemJsonObj.getString("unit");
+                                    ingredientString = ingredientString + " " + itemJsonObj.getString("name");
                                 }
                                 if (!itemJsonObj.getString("note").equals("null")) {
                                     ingredientString = ingredientString + " " + itemJsonObj.getString("note");
@@ -333,9 +333,9 @@ public class SingleRecipeActivity extends Activity {
                             for (int k = 0; k < itemsJsonArray.length(); k++) {
                                 itemJsonObj = itemsJsonArray.getJSONObject(k);
 
-                                ingredientString = ingredientString + itemJsonObj.getString("name");
+                                ingredientString = ingredientString + itemJsonObj.getString("unit");
                                 if (!itemJsonObj.getString("unit").equals("null")) {
-                                    ingredientString = ingredientString + " " + itemJsonObj.getString("unit");
+                                    ingredientString = ingredientString + " " + itemJsonObj.getString("name");
                                 }
                                 if (!itemJsonObj.getString("note").equals("null")) {
                                     ingredientString = ingredientString + " " + itemJsonObj.getString("note");
@@ -351,9 +351,9 @@ public class SingleRecipeActivity extends Activity {
                             for (int k = 0; k < itemsJsonArray.length(); k++) {
                                 itemJsonObj = itemsJsonArray.getJSONObject(k);
 
-                                ingredientString = ingredientString + itemJsonObj.getString("name");
+                                ingredientString = ingredientString + itemJsonObj.getString("unit");
                                 if (!itemJsonObj.getString("unit").equals("null")) {
-                                    ingredientString = ingredientString + " " + itemJsonObj.getString("unit");
+                                    ingredientString = ingredientString + " " + itemJsonObj.getString("name");
                                 }
                                 if (!itemJsonObj.getString("note").equals("null")) {
                                     ingredientString = ingredientString + " " + itemJsonObj.getString("note");
