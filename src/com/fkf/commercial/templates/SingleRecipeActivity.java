b8ea890ccbe9127521d580200b9a7df99df39d4f
Kavimal Wijewardana <kavimal@thinkcube.com>
2014-03-22 22:41:52 +0530

    Fixed the alignment issue in recipe title and rating stars. Add ingredient name single ingredient

--- src/com/fkf/commercial/templates/SingleRecipeActivity.java
+++ src/com/fkf/commercial/templates/SingleRecipeActivity.java
@@ -263,6 +263,7 @@ public class SingleRecipeActivity extends Activity {
                 JSONObject itemJsonObj = null;
                 if (ingredientJsonArray.length() == 1) {
                     ingredientJsonObj = ingredientJsonArray.getJSONObject(0);
+                    singleRecipeContentLabelTextView.setText(ingredientJsonObj.getString("title"));
                     itemsJsonArray = ingredientJsonObj.getJSONArray("items");
                     for (int j = 0; j < itemsJsonArray.length(); j++) {
                         itemJsonObj = itemsJsonArray.getJSONObject(j);
@@ -417,7 +418,7 @@ public class SingleRecipeActivity extends Activity {
                 ingredientLinearLayout.addView(ingredientNoteTextView);
             }
 
-            separatorLineTextView.setText("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+            separatorLineTextView.setText("- - - - - - - - - - - - - - - - - - - - - - - - - - -");
             separatorLineTextView.setTextColor(getResources().getColor(R.color.line_color));
             ingredientLinearLayout.addView(separatorLineTextView);
         } catch (JSONException ex) {
