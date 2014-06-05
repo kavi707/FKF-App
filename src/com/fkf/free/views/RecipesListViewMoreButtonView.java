fa9843f0c05938ae88427ce5fc99ff2725faeace
Kavimal Wijewardana <kavimal@thinkcube.com>
2014-06-05 20:20:14 +0530

    Add message balloon to more recipes button

--- src/com/fkf/free/views/RecipesListViewMoreButtonView.java
+++ src/com/fkf/free/views/RecipesListViewMoreButtonView.java
@@ -1,6 +1,8 @@
 package com.fkf.free.views;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.AttributeSet;
@@ -19,6 +21,7 @@ public class RecipesListViewMoreButtonView extends LinearLayout {
 
     private ImageButton moreRecipesBtn;
     private Context context;
+    private AlertDialog messageBalloonAlertDialog;
 
     public RecipesListViewMoreButtonView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -34,11 +37,43 @@ public class RecipesListViewMoreButtonView extends LinearLayout {
             @Override
             public void onClick(View v) {
                 String appPackageName = "com.fkf.commercial";
-                try {
-                    context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
-                }
+
+                messageBalloonAlertDialog = new AlertDialog.Builder(context)
+                        .setTitle(R.string.full_version_label)
+                        .setMessage(R.string.buy_pro_message_2)
+                        .setPositiveButton(R.string.buy_label, new AlertDialog.OnClickListener() {
+                            /**
+                             * This method will be invoked when a button in the dialog is clicked.
+                             *
+                             * @param dialog The dialog that received the click.
+                             * @param which  The button that was clicked (e.g.
+                             *               {@link android.content.DialogInterface#BUTTON1}) or the position
+                             */
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String appPackageName = "com.fkf.commercial";
+
+                                try {
+                                    context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                                } catch (android.content.ActivityNotFoundException ex) {
+                                    context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+                                }
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new AlertDialog.OnClickListener() {
+                            /**
+                             * This method will be invoked when a button in the dialog is clicked.
+                             *
+                             * @param dialog The dialog that received the click.
+                             * @param which  The button that was clicked (e.g.
+                             *               {@link android.content.DialogInterface#BUTTON1}) or the position
+                             */
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                messageBalloonAlertDialog.cancel();
+                            }
+                        }).create();
+                messageBalloonAlertDialog.show();
             }
         });
     }
